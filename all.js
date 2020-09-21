fd82fd470a077a533a658ad518b4702fb45e6b2a
Steve <ycsteven.tsao@gmail.com>
2020-09-22 03:59:49 +0800

    add js feature 2

--- all.js
+++ all.js
@@ -1,6 +1,14 @@
-for (var i = 3; i < 16; i++) {
-  for (var j = 1; j < 16; j++) {
-    let k = i * j;
-    console.log(i + "x" + j + "=" + k);
-  }
+// for (var i = 3; i < 16; i++) {
+//   for (var j = 1; j < 16; j++) {
+//     let k = i * j;
+//     console.log(i + "x" + j + "=" + k);
+//   }
+// }
+
+let w;
+let h;
+function calBmi(w, h) {
+  let num = parseFloat(w / Math.pow(h / 100, 2));
+  console.log(num);
 }
+calBmi(70, 177);
